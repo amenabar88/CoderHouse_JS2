6156ac6c4064ab0bae6f1bd8d292b8840de3d587
amenabar88 <joaquinjararamirez05@gmai.com>
2024-02-21 18:01:38 -0300

    re entrega de la Pre Entrega 1 oficial

--- js/main.js
+++ js/main.js
@@ -1,19 +1,48 @@
-alert("El mejor lugar para solicitar prestamos")
-let menu = false
-let edad = prompt("por favor, indique su edad")
-    if(edad >= 18){
-    let edad = alert ("Es mayor de edad, puede continuar")
-    menu = true
-    }else {
-    alert("Error. Debes ser mayor de edad para solicitar un prestamo")
+function verificarEdad (edad){
+    return edad >= 18;
 }
 
-if (menu){
-    let monto = prompt ("ingrese el monto que desea:")
-    let cuotas = prompt ("ingrese la cantidad de cuotas:")
-    if (monto <= 0 || cuotas <= 0){
-        alert ("por favor, ingrese valores validos")
+function valores (monto, cuotas){
+    return monto > 0 && cuotas > 0;
+}
+
+function calcularIntereses (monto, cuotas){
+    const tasaInteresAnual = 0.1;
+    const totalItereses = monto * tasaInteresAnual * cuotas;
+    return totalItereses
+}
+
+do{
+    alert ("El mejor lugar para solicitar un prestamo");
+
+    let menu = false
+    let edad = prompt ("Por favor, indique su edad");
+
+    if (verificarEdad(edad)){
+        alert("Es mayor de edad, puede continuar");
+        menu = true
+    } else{
+        alert ("Error. Debes ser mayor de edad para solicitar un prestamo")
     }
-
-
-}
+    if (menu){
+        let monto;
+        let cuotas;
+        do{
+            monto = prompt ("Ingrese el monto que desea:");
+            cuotas = prompt ("Ingrese la cantidad de cuotas");
+        }while (!valores(monto, cuotas));
+        const interes = calcularIntereses (parseInt(monto), parseInt (cuotas));
+    
+        const montoTotal = parseInt(monto) + interes;
+        
+        alert (`El interés total del préstamo es: ${interes}`);
+        
+        alert (`El monto total del préstamo es: ${montoTotal}`);
+    
+        const continuar = confirm("¿Desea realizar otra operación?")
+        if (!continuar){
+            alert ("gracias por usar nuestra plataforma. Que tenga buen dia");
+            break;
+    }
+    }
+}while (true);
